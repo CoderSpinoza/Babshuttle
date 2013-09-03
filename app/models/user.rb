7b77a92355b2c95b3b22c0dfba8307bf08295776
Hara Kang <hara0115@gmail.com>
2013-09-02 21:58:34 -0700

    Changed welcome email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,18 @@ class User < ActiveRecord::Base
   has_many :addresses
   has_many :orders
   after_create :send_welcome_email
-
-  private
-	  def app_params
-	    params.require(:user).permit(:name, :address, :email, :updated_at, :created_at)
-	  end
-
-	  def send_welcome_email
+  def send_welcome_email
       # StatsMix.api_key = ENV['STATSMIX_API_KEY']
       # StatsMix.track("User Growth", User.count)
       # if StatsMix.error
       #   puts "Error: #{StatsMix.error}"
       # end
 
-	  	UserMailer.welcome(self).deliver
-	  end
+    UserMailer.welcome(self).deliver
+  end
+
+  private
+	  def app_params
+	    params.require(:user).permit(:name, :address, :email, :updated_at, :created_at)
+	  end  
 end
