d096a157b0722b2deac03b0ebf9890c577bdfef0
Hara Kang <hara0115@gmail.com>
2013-09-09 22:36:06 -0700

    Ordered by created_at

--- spec/factories/addresses.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :address do
-    street "MyString"
-    apt "MyString"
-    zipcode "MyString"
-  end
-end
